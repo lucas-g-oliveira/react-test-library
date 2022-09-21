1beca9690f886ea4a13a239538daabff9f6f09ef
Lucas G <lucasg.devmail93@gmail.com>
2022-09-21 17:50:04 -0300

    requisito 6 -> Passou!

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -7,8 +8,24 @@ describe('Testes do requisito 6 - componente <Pokemon.js />', () => {
   beforeEach(() => renderWithRouter(<App />));
 
   it('Testa o atributo "alt" do pokemon', () => {
-    const notFoundPage = screen.getByRole('img', {
-      name: /pikachu crying because the page requested was not found/i });
-    expect(notFoundPage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    const imageName = screen.getByTestId('pokemon-name');
+    const imagePokemon = screen.getByRole('img', { name: /pikachu sprite/i });
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemomWeight = screen.getByTestId('pokemon-type');
+
+    const linkMoreDeatails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(linkMoreDeatails);
+    const check = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(check);
+    const favoriteFeedback = screen
+      .getByRole('img', { name: /pikachu is marked as favorite/i });
+
+    expect(imagePokemon).not.toHaveAttribute('src', '');
+    expect(imagePokemon).not.toHaveAttribute('alt', '');
+    expect(favoriteFeedback).not.toHaveAttribute('src', '');
+    expect(favoriteFeedback).toBeInTheDocument();
+    expect(imageName.textContent).not.toBe('');
+    expect(pokemonType.textContent).not.toBe('');
+    expect(pokemomWeight.textContent).not.toBe('');
   });
 });
